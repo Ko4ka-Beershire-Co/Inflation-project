78971fda9cd4803bb4f7bf864a176e0f3e72f8e3
Alexander Kuznetsov <alex@beershire.ru>
2021-04-15 11:56:36 +0300

    Create AdidasSuperstar.py

---
+++ Parser_folder/AdidasSuperstar.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.adidas.ru/krossovki-superstar/FV3284.html'
+HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
+item_1 = 'div'
+item_2 = 'gl-price-item notranslate'
+
+
+def parser(URL, HEADERS, item_1, item_2):
+    # Request to send
+    def get_html(url, params=None):
+        r = requests.get(url, headers=HEADERS, params=params)
+        return r
+
+    # Parse html tree and return class containing the price
+    def get_content(html):
+        soup = BeautifulSoup(html, 'html.parser')
+        items = soup.find(item_1, class_=item_2)
+        return items.text[:-2]
+
+    # If error check and get full html tree
+    def parse():
+        html = get_html(URL)
+        if html.status_code == 200:
+            j = get_content(html.text)
+        else:
+            print('Чет не работает нихрена...')
+            j = 'Error'
+        return j
+
+    return str(parse())
+
+
+if __name__ == "__main__":
+
+    print(parser(URL, HEADERS, item_1, item_2))
