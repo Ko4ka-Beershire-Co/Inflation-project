f20971e4013271a5e1e57311670fc520e802f90d
Alexander Kuznetsov <alex@beershire.ru>
2021-04-15 16:58:59 +0300

    Update Appartaments.py

--- Parcer_folder_pipeline/Appartaments.py
+++ Parcer_folder_pipeline/Appartaments.py
@@ -2,14 +2,12 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-URL = 'https://msk.etagi.com/analytics/'
-HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
-item_1 = 'div'
-item_2 = '_2zoh'
-
-
 def parser(URL, HEADERS, item_1, item_2):
+    URL = 'https://msk.etagi.com/analytics/'
+    HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
+    item_1 = 'div'
+    item_2 = '_2zoh' 
     # Request to send
     def get_html(url, params=None):
         r = requests.get(url, headers=HEADERS, params=params)
