674337cb6348f413776b35eaf93105db23ef6348
Alexander Kuznetsov <alex@beershire.ru>
2021-04-15 19:26:51 +0300

    Update Timberland.py

--- Parcer_folder_pipeline/Timberland.py
+++ Parcer_folder_pipeline/Timberland.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-URL = 'https://timberland.ru/catalog/men/men_obuv/men_botinki/botinki_6_inch_premium_boot_tbl10061w/?color=yellow'
-HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+
+def parser():
+  
+    URL = 'https://timberland.ru/catalog/men/men_obuv/men_botinki/botinki_6_inch_premium_boot_tbl10061w/?color=yellow'
+    HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
-item_1 = 'div'
-item_2 = 'item__data'
-
-
-def parser(URL, HEADERS, item_1, item_2):
+    item_1 = 'div'
+    item_2 = 'item__data'
     # Request to send
     def get_html(url, params=None):
         r = requests.get(url, headers=HEADERS, params=params)
@@ -37,4 +37,4 @@ def parser(URL, HEADERS, item_1, item_2):
 
 if __name__ == "__main__":
 
-    parser(URL, HEADERS, item_1, item_2)
+    parser()
