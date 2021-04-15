6e19048c318f758be8e88f036de4dd418d7357fb
Alexander Kuznetsov <alex@beershire.ru>
2021-04-15 22:56:30 +0300

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -12,23 +12,44 @@ from bs4 import BeautifulSoup
 import os
 import shutil
 import importlib
+import time
+
+# Multithreading
+import multiprocessing
+import time
 
 parser_location = "C://Users/Alex/Desktop/Python/Inflation/Inflation-project-master/Parcer_folder_pipeline"
 parser_list = []
 k = 0
 
+#
+
 # List all Parser in the target folder
 for filename in os.listdir(parser_location):
     parser_list.append(filename)
 
-print(parser_list)
 # Parse and push script
 for k in parser_list[:-1]:  # -1 deletes _pycache_
     # from package import name as imported
-    package = "Parcer_folder_pipeline."+k[:-3]
+    package = "Parcer_folder_pipeline." + k[:-3]
     name = "parser"
-    # BLYAAAAA
-    imported = getattr(__import__(package, fromlist=[name]), name)
-    print(str(k))
-    print(imported())
 
+    # Very blya complicated
+    imported = getattr(__import__(package, fromlist=[name]), name)
+    try:
+        if __name__ == '__main__':
+            # kick-in multithreading
+            thread = multiprocessing.Process(target=print(imported()))
+            thread.start()
+            # Timeout
+            thread.join(20)
+            # What it time > X(join)
+            if thread.is_alive():
+                print('Timeout')
+                thread.terminate()  # Soft
+                # thread.kill()  # Hard
+                # Finish
+                thread.join()
+    except OSError:
+        print(str(k))
+        print('Error')
