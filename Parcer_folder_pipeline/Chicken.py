1f9d8dc00383a91a52a8d7b5782fe7af317e2d1b
Alexander Kuznetsov <alex@beershire.ru>
2021-04-15 17:04:21 +0300

    Update Chicken.py

--- Parcer_folder_pipeline/Chicken.py
+++ Parcer_folder_pipeline/Chicken.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://lenta.com/product/pf-kurinoe-file-s-grudki-ohl-ves-rossiya-011991/'
-HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
-item_1 = 'span'
-item_2 = 'sku-price__integer'
-
-
 def parser(URL, HEADERS, item_1, item_2):
+  
+    URL = 'https://lenta.com/product/pf-kurinoe-file-s-grudki-ohl-ves-rossiya-011991/'
+    HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
+    item_1 = 'span'
+    item_2 = 'sku-price__integer'
+    
     # Request to send
     def get_html(url, params=None):
         r = requests.get(url, headers=HEADERS, params=params)
