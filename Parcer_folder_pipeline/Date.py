023c4029baf3d23cd60bdb26951cb93b719ee638
Alexander Kuznetsov <alex@beershire.ru>
2021-04-21 17:20:14 +0300

    Update Date.py

--- Parcer_folder_pipeline/Date.py
+++ Parcer_folder_pipeline/Date.py
@@ -3,9 +3,10 @@ import datetime
 
 def parser():
     i = datetime.date.today()
-    j = i.strftime('%m/%d/20%y')
+    j = i.strftime('%m/%y')
     return j
 
 
 if __name__ == "__main__":
     parser()
+
