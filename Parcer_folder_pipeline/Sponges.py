a14f023b59e31ad94b4601163ec16b71b279fad0
Alexander Kuznetsov <alex@beershire.ru>
2021-06-07 10:33:32 +0300

    Update Sponges.py

--- Parcer_folder_pipeline/Sponges.py
+++ Parcer_folder_pipeline/Sponges.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 
 
 def parser():
-  
-    URL = 'https://www.auchan.ru/product/gubka-dlya-chistki-scotch-brite-delikat-formovannaya-2-sht/'
+    URL = 'https://www.auchan.ru/product/gubka_dlya_mytya_10sht/'
     HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
+                             'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
     item_1 = 'div'
     item_2 = 'fullPricePDP css-1129a1l'
+
     # Request to send
     def get_html(url, params=None):
         r = requests.get(url, headers=HEADERS, params=params)
@@ -30,9 +30,8 @@ def parser():
             j = 'Error'
         return j
 
-    return str(parse())
+    return round(float(parse()) * 5.27)  # Replacement fix
 
 
 if __name__ == "__main__":
-
     parser()
