811a5b37f0b4f16bf23a87a27a36cbe499cedde2
Alexander Kuznetsov <alex@beershire.ru>
2021-07-05 11:17:34 +0300

    Update Shampoo.py

--- Parcer_folder_pipeline/Shampoo.py
+++ Parcer_folder_pipeline/Shampoo.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def parser():
-  
-    URL = 'https://www.auchan.ru/product/shampun-dlya-volos-head-shoulders-osnovnoy-uhod-400-ml/'
+    URL = 'https://msk.metro-cc.ru/category/kosmetika/uhod-za-volosami/shampuni-balzamy-maski/art_23476'
     HEADERS = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
+                             'Chrome/84.0.4147.105 Safari/537.36', 'accept': '*/*'}  # Real params
     item_1 = 'div'
-    item_2 = 'fullPricePDP css-1129a1l'
+    item_2 = 'price-card__price'
+
     # Request to send
     def get_html(url, params=None):
         r = requests.get(url, headers=HEADERS, params=params)
@@ -18,7 +19,8 @@ def parser():
     def get_content(html):
         soup = BeautifulSoup(html, 'html.parser')
         items = soup.find(item_1, class_=item_2)
-        return items.text[0:6]
+        items = re.findall(r'<span itemprop="price">\n\s*(\d\d\d)', str(items))
+        return items[0]
 
     # If error check and get full html tree
     def parse():
@@ -34,5 +36,4 @@ def parser():
 
 
 if __name__ == "__main__":
-
     parser()
